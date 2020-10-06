d30ad5efe35fc9a712252a2e3503ce5c2796b13c
Diogo Santos <diogofilipe1_santos@hotmail.com>
2020-10-06 17:53:28 +0100

    Ja estabelece ligacao, falta verificar timeOuts

--- tp1/noncanonical.c
+++ tp1/noncanonical.c
@@ -5,6 +5,9 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define BAUDRATE B38400
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
@@ -13,103 +16,108 @@
 
 volatile int STOP=FALSE;
 
+int receiveConnectionRequest(int fd){
+  unsigned char buf;
+  read(fd, &buf, 1); // reads f
+  
+  if(buf == 0x7E){
+      read(fd, &buf, 1); // reads a
+      read(fd, &buf, 1); // reads c
+      read(fd, &buf, 1); // reads bcc
+      if(buf == (0x03 ^ 0x03)){
+        read(fd, &buf ,1); // reads f
+        if(buf == 0x7E){
+          printf("\nMensagem Recebida!\n\nEnviando Confirmacao...\n");
+          return 1;
+        }
+      }
+  }
+  
+  return 0;
+}
+
+void writeAcknowledge(int fd){
+  unsigned char f = 0x7E, a = 0x01, c = 0x07, bcc = a ^ c;
+
+  write(fd, &f, 1);
+  write(fd, &a, 1);
+  write(fd, &c, 1);
+  write(fd, &bcc, 1);
+  write(fd, &f, 1);
+
+  printf("Acknowledgment Enviado!\n\n");
+}
+
 int main(int argc, char** argv)
 {
-    int fd,c, res;
-    struct termios oldtio, newtio;
-    unsigned char buf;
+  int fd;
+  struct termios oldtio, newtio;
 
-    if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS11\n");
-      exit(1);
-    }
-
+  if ( (argc < 2) || 
+        ((strcmp("/dev/ttyS10", argv[1])!=0) && 
+        (strcmp("/dev/ttyS11", argv[1])!=0) )) {
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS11\n");
+    exit(1);
+  }
 
   /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
   */
-  
     
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
+  fd = open(argv[1], O_RDWR | O_NOCTTY );
+  if (fd <0) {perror(argv[1]); exit(-1); }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
+  if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
 
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
 
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
-
-
+  newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
+  newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
 
   /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
     leitura do(s) pr�ximo(s) caracter(es)
   */
 
+  tcflush(fd, TCIOFLUSH);
+
+  if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+
+  printf("New termios structure set\n");
+  printf("\n==============\n");
+
+  //================================
+
+  if(receiveConnectionRequest(fd)){
+      writeAcknowledge(fd);
+  }
+
+  //================================
 
 
-    tcflush(fd, TCIOFLUSH);
+  /*num = 0;
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
+  while(num < strlen(buf)){
+    write(fd, &buf[num], 1);
+    num++;
+  }
+  buf[strlen(buf)] = '\0';
+  write(fd, &buf[strlen(buf)], 1);
 
-    printf("New termios structure set\n");
-
-    int send = 0;
-    
-    read(fd,&buf,1); // reads f
-    
-    if(buf == 0x7E){
-        printf("first f");
-        read(fd,&buf,1); // reads a
-        read(fd,&buf,1); // reads c
-        read(fd,&buf,1); // reads bcc
-        if(buf == 0x03 ^ 0x03){
-            printf("bcc");
-            read(fd,&buf,1); // reads f
-            if(buf == 0x7E){
-                printf("second f");
-                send = 1;
-                printf("Mensagem Recebida: %s\n\nEnviando       Confirmacao...\n", buf);
-            }
-        }
-    }
-
-    if(send){
-        unsigned char f = 0x7E, a = 0x01, c = 0x07, bcc = a ^ c;
-        write(fd, &f, 1);
-        write(fd, &a, 1);
-        write(fd, &c, 1);
-        write(fd, &bcc, 1);
-        write(fd, &f, 1);
-    }
-
-
-    /*num = 0;
-
-    while(num < strlen(buf)){
-      write(fd, &buf[num], 1);
-      num++;
-    }
-    buf[strlen(buf)] = '\0';
-    write(fd, &buf[strlen(buf)], 1);
-
-    printf("Mensagem Enviada!\n");*/
+  printf("Mensagem Enviada!\n");*/
 
 
 
@@ -117,9 +125,11 @@ int main(int argc, char** argv)
     O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o 
   */
 
+  printf("==============\n\n");
 
-    sleep(1);
-    tcsetattr(fd,TCSANOW,&oldtio);
-    close(fd);
-    return 0;
+  sleep(1);
+  tcsetattr(fd,TCSANOW,&oldtio);
+  close(fd);
+
+  return 0;
 }
--- tp1/writenoncanonical.c
+++ tp1/writenoncanonical.c
@@ -5,6 +5,9 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
@@ -14,133 +17,151 @@
 
 volatile int STOP=FALSE;
 
+int tentativas= 0, fd;
+
+void establishConnection(){
+  if(tentativas != 3){
+    unsigned char f = 0x7E, a = 0x03, c = 0x03, bcc = a ^ c;
+
+    write(fd, &f, 1);
+    write(fd, &a, 1);
+    write(fd, &c, 1);
+    write(fd, &bcc, 1);
+    write(fd, &f, 1);
+
+    printf("\nPedido de Conexao Enviado!\n\nEspera por Acknowledgment...\n");
+    alarm(3); // quantidade de tempo que espera pelo acknowledgment
+  }
+  else{
+    printf("\nPedido de Conexao Nao Enviado!\nAtingiu o limite de tentativas!\n\n");
+  }
+  tentativas++;
+}
+
+void waitingAcknowledgment(){
+  unsigned char buf;
+
+  /*read(fd,&buf,1); // reads f
+  if(buf == 0x7E){
+    read(fd, &buf, 1); // reads a
+    read(fd, &buf, 1); // reads c
+    read(fd, &buf, 1); // reads bcc
+    if(buf == (0x01 ^ 0x07)){
+        read(fd, &buf, 1); // reads f
+        if(buf == 0x7E) {
+          printf("Acknowledgment Recebido!\n\n");
+        }
+    }
+  }*/
+
+  do{
+    read(fd, &buf, 1);
+  } while(buf == (0x01 ^ 0x07)); // verifica se a trama recebida e valida
+
+  printf("Acknowledgment Recebido!\n\n");
+  alarm(0); // cancela todos os alarmes pendentes
+}
+
 int main(int argc, char** argv)
 {
-    int fd,c, res;
-    struct termios oldtio,newtio;
-    char buf[255];
-    int i, sum = 0, speed = 0;
-    
-    if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS11\n");
-      exit(1);
-    }
-
+  struct termios oldtio,newtio;
+  
+  if ( (argc < 2) || 
+        ((strcmp("/dev/ttyS10", argv[1])!=0) && 
+        (strcmp("/dev/ttyS11", argv[1])!=0) )) {
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS11\n");
+    exit(1);
+  }
 
   /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
   */
 
+  fd = open(argv[1], O_RDWR | O_NOCTTY );
+  if (fd <0) {perror(argv[1]); exit(-1); }
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
+  if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
+  /*
+  Configuração da Porta Série
+  */
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
 
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
 
-    /*
-    Configuração da Porta Série
-    */
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
-
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
-
-    newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+  newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
+  newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
 
   /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
     leitura do(s) pr�ximo(s) caracter(es)
   */
 
+  tcflush(fd, TCIOFLUSH);
 
+  if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
-    tcflush(fd, TCIOFLUSH);
+  printf("New termios structure set\n");
+  printf("\n==============\n");
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
+  /*printf("Insert string: ");
+  
+  if(fgets(buf, 255, stdin) == NULL){
+    printf("Erro!\n");
+    exit(1);
+  }
+  
+  int num = 0;
+  while(num < strlen(buf)){
+    write(fd, &buf[num], 1);
+    num++;
+  }
+  buf[strlen(buf)] = '\0';
+  write(fd, &buf[strlen(buf)], 1);*/
+  //printf("Mensagem Enviada!\nEspera por Confirmacao...\n\n");
 
-    printf("New termios structure set\n");
+  //=================
 
-    /*printf("Insert string: ");
-    
-    if(fgets(buf, 255, stdin) == NULL){
-      printf("Erro!\n");
-      exit(1);
-    }
-    
-    int num = 0;
-    while(num < strlen(buf)){
-      write(fd, &buf[num], 1);
-      num++;
-    }
-    buf[strlen(buf)] = '\0';
-    write(fd, &buf[strlen(buf)], 1);*/
+  (void) signal(SIGALRM, establishConnection);
 
-    unsigned char flag = 0x7E;
-    write(fd, &flag, 1);
-    unsigned char a = 0x03;
-    write(fd, &a, 1);
-    unsigned char c1 = 0x03;
-    write(fd, &c1, 1);
-    unsigned char bcc = a ^ c1;
-    write(fd, &bcc, 1);
-    write(fd, &flag, 1);
+  establishConnection(); // sends request to establish connection
+  waitingAcknowledgment(); // waits for the receiver acknowledgment
 
-    printf("Mensagem Enviada!\nEspera por Confirmacao...\n\n");
+  //=================
 
-    /*num = 0;
-    while(1){
-      read(fd, &buf[num], 1);
-      if(buf[num] == '\0') break;
-      num++;
-    }
+  /*num = 0;
+  while(1){
+    read(fd, &buf[num], 1);
+    if(buf[num] == '\0') break;
+    num++;
+  }
 
-    printf("Mensagem Recebida: %s\n", buf);*/
- 
+  printf("Mensagem Recebida: %s\n", buf);*/
 
   /* 
     O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar 
     o indicado no gui�o 
   */
-    int received = 0;
 
-    read(fd,&buf,1); // reads f
-    if(buf == 0x7E){
-        read(fd,&buf,1); // reads a
-        read(fd,&buf,1); // reads c
-        read(fd,&buf,1); // reads bcc
-        if(buf == 0x01 ^ 0x07){
-            read(fd,&buf,1); // reads f
-            if(buf == 0x7E) received = 1;
-        }
-    }
+  printf("==============\n\n");
 
-    if(received) printf("Mensagem Recebida: %s\n", buf);
+  if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
+  close(fd);
 
-
-   
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
-
-
-
-
-    close(fd);
-    return 0;
+  return 0;
 }
